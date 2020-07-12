876840ab587faa256a63f228e945eecc2cb074c7
Vincent Robles <sylkisgsx@gmail.com>
2020-07-12 08:06:26 -0400

    created portal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
 import Toggle from './components/Toggle/Toggle';
-import HelloWorld from './HelloWorld';
+import HelloWorld from './components/HelloWorld/HelloWorld';
+import Portal from './components/Portal/Portal';
 
 function App() {
   return (
     <div className='App'>
       <Toggle>
         {({ on, toggle }) => (
-          <div>
+          <>
             {on && <HelloWorld on={on} />}
             <button onClick={toggle}>Show/Hide</button>
-          </div>
+            <Portal>{on && <h1>Hi, I'm in a portal!</h1>}</Portal>
+          </>
         )}
       </Toggle>
     </div>
